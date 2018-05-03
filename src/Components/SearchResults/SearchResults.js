10acfcfbeda5c59fd64b83e0d92bca8b8fb80316
grosenb <grosenb@cisco.com>
2018-05-03 17:26:01 +1000

    create mockup of page forward and backward buttons with tooltips

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './SearchResults.css';
+import './tooltips.css';
 import TrackList from '../TrackList/TrackList';
 
 class SearchResults extends React.Component {
@@ -9,6 +10,10 @@ class SearchResults extends React.Component {
       <div className="SearchResults">
         <h2>Results</h2>
         <TrackList tracks={this.props.searchResults} isRemoval={isRemoval} onAdd={this.props.onAdd} />
+        <div className="pageNav">
+          <button tooltip="previous results" flow="right" className="triangle-left"></button>
+          <button tooltip="next results" flow="left" className="triangle-right"></button>
+        </div>
       </div>
     );
   }
